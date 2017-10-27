8fd0eb21d658c58a5d092adb24f7d68549598645
Leonard-happy <leonard.happy@kakaocorp.com>
2017-10-28 01:03:20 +0900

    IssuesViewController 만들기

---
+++ IssuesApp/View/IssuesViewController.swift
@@ -0,0 +1,48 @@
+//
+//  IssuesViewController.swift
+//  IssuesApp
+//
+//  Created by Leonard on 2017. 10. 28..
+//  Copyright © 2017년 intmain. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class IssuesViewController: UIViewController {
+
+    let owner = GlobalState.instance.owner
+    let repo = GlobalState.instance.repo
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        App.api.repoIssues(owner: owner, repo: repo, page: 1, handler: { (response: DataResponse<[Model.Issue]>) in
+            switch response.result {
+            case .success(let items):
+                print("issues: \(items)")
+            case .failure:
+                break
+            }
+        })
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- IssuesApp/View/RepoViewController.swift
+++ IssuesApp/View/RepoViewController.swift
@@ -15,8 +15,9 @@ class RepoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        ownerTextField.text = GlobalState.instance.owner
+        repoTextField.text = GlobalState.instance.repo
+        // Do any additional setup after loadingJ the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,15 +29,20 @@ class RepoViewController: UIViewController {
     
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        guard let owner = ownerTextField.text, let repo = repoTextField.text else { return false }
+        return !(owner.isEmpty || repo.isEmpty)
+    }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "EnterRepoSegue" {
+            guard let owner = ownerTextField.text, let repo = repoTextField.text else { return }
+            GlobalState.instance.owner = owner
+            GlobalState.instance.repo = repo
+        }
     }
  
-    
-    
-    
 }
 
 extension RepoViewController {

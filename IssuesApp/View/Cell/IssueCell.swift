243e30d943e894a9ad1c2d188e9e5f2af39a4df0
Leonard-happy <leonard.happy@kakaocorp.com>
2017-11-01 22:29:09 +0900

    IssueCell 추가.

---
+++ IssuesApp/View/Cell/IssueCell.swift
@@ -0,0 +1,46 @@
+//
+//  IssueCell.swift
+//  IssuesApp
+//
+//  Created by Leonard on 2017. 11. 1..
+//  Copyright © 2017년 intmain. All rights reserved.
+//
+
+import UIKit
+
+class IssueCell: UICollectionViewCell {
+    @IBOutlet var stateButton: UIButton!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var contentLabel: UILabel!
+    @IBOutlet var commentCountButton: UIButton!
+}
+
+extension IssueCell {
+    static var cellFromNib: IssueCell {
+        guard let cell = Bundle.main.loadNibNamed("IssueCell", owner: nil, options: nil)?.first as? IssueCell else {
+            return IssueCell()
+        }
+        return cell
+    }
+    
+    func update(data issue: Model.Issue) {
+        titleLabel.text = issue.title
+        contentLabel.text = issue.body
+        let createdAt = issue.createdAt?.string(dateFormat: "dd MMM yyyy") ?? "-"
+        contentLabel.text = "#\(issue.number) \(issue.state.rawValue) on \(createdAt) by \(issue.user.login)"
+        commentCountButton.setTitle("\(issue.comments)", for: .normal)
+        stateButton.isSelected = issue.state == .closed
+        let commentCountHidden: Bool = issue.comments == 0
+        commentCountButton.isHidden = commentCountHidden
+    }
+}
+
+extension Date {
+    func string(dateFormat: String, locale: String = "en-US") -> String {
+        let format = DateFormatter()
+        format.dateFormat = dateFormat
+        format.locale = Locale(identifier: locale)
+        return format.string(from: self)
+    }
+}
+

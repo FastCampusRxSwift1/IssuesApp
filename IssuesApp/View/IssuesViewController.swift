565e952b3cbe06db80194a451506c4d881ffda9e
Leonard-happy <leonard.happy@kakaocorp.com>
2017-11-01 22:57:17 +0900

    issuesViewController에 datasource 연결해서 Cell 보이기.

--- IssuesApp/View/IssuesViewController.swift
+++ IssuesApp/View/IssuesViewController.swift
@@ -14,6 +14,7 @@ class IssuesViewController: UIViewController {
     let owner = GlobalState.instance.owner
     let repo = GlobalState.instance.repo
     var datasource: [Model.Issue] = []
+    @IBOutlet var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,14 +43,19 @@ class IssuesViewController: UIViewController {
 
 extension IssuesViewController {
     func setup() {
+        collectionView.dataSource = self
+//        collectionView.delegate = self
+        collectionView.register(UINib(nibName: "IssueCell", bundle: nil), forCellWithReuseIdentifier: "IssueCell")
         load()
     }
     
     func load() {
-        App.api.repoIssues(owner: owner, repo: repo, page: 1, handler: { (response: DataResponse<[Model.Issue]>) in
+        App.api.repoIssues(owner: owner, repo: repo, page: 1, handler: { [weak self] (response: DataResponse<[Model.Issue]>) in
+            guard let `self` = self else { return }
             switch response.result {
             case .success(let items):
                 print("issues: \(items)")
+                self.dataLoaded(items: items)
             case .failure:
                 break
             }
@@ -57,6 +63,22 @@ extension IssuesViewController {
     }
     
     func dataLoaded(items: [Model.Issue]) {
-        
+        datasource = items
+        collectionView.reloadData()
+    }
+}
+
+extension IssuesViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return datasource.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "IssueCell", for: indexPath) as? IssueCell else { return UICollectionViewCell() }
+        let item = datasource[indexPath.item]
+        cell.update(data: item)
+        return cell
     }
 }

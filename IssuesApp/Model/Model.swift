9e39b6e779f134b5070112c384905e48ec49412d
Leonard-happy <leonard.happy@kakaocorp.com>
2017-11-07 23:17:23 +0900

    Comment 모델 추가.

--- IssuesApp/Model/Model.swift
+++ IssuesApp/Model/Model.swift
@@ -65,3 +65,22 @@ extension Model {
         }
     }
 }
+
+extension Model {
+    public struct Comment {
+        let id: Int
+        let user: Model.User
+        let body: String
+        let createdAt: Date?
+        let updatedAt: Date?
+        public init(json: JSON) {
+            id = json["id"].intValue
+            user = Model.User(json: json["user"])
+            body = json["body"].stringValue
+            let format = DateFormatter()
+            format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
+            createdAt = format.date(from: json["created_at"].stringValue)
+            updatedAt = format.date(from: json["updated_at"].stringValue)
+        }
+    }
+}

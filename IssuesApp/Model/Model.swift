33f47a208cdafee0ab20176542d9bc32f4b0dc55
Leonard-happy <leonard.happy@kakaocorp.com>
2017-11-10 22:53:34 +0900

    model까지 blank 처리

--- IssuesApp/Model/Model.swift
+++ IssuesApp/Model/Model.swift
@@ -52,72 +52,6 @@ extension Model.Issue {
     }
 }
 
-extension Model.Issue.State {
-    var color: UIColor {
-        switch  self {
-        case .open:
-            return UIColor(red: 131/255, green: 189/255, blue: 71/255, alpha: 1)
-        case .closed:
-            return UIColor(red: 176/255, green: 65/255, blue: 32/255, alpha: 1)
-        }
-    }
-}
-
-extension Model.Issue {
-    var toDict: [String: Any] {
-        let format = DateFormatter()
-        format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-        var dict: [String : Any] = [
-            "id": id,
-            "number": number,
-            "title": title,
-            "comments": comments,
-            "body": body,
-            "state": state.rawValue,
-            "user": [
-                "id": user.id,
-                "login": user.login,
-                "acatar_url": (user.avatarURL?.absoluteString ?? "")]
-        ]
-        if let createdAt = createdAt {
-            dict["createdAt"] = format.string(from: createdAt)
-        }
-        if let updatedAt = updatedAt {
-            dict["updatedAt"] = format.string(from: updatedAt)
-        }
-        if let closedAt = closedAt {
-            dict["closedAt"] = format.string(from: closedAt)
-        }
-        
-        print("dict: \(dict)")
-        return dict
-        
-    }
-}
-
-extension Model.Issue: Equatable {
-    static func ==(lhs: Model.Issue, rhs: Model.Issue) -> Bool {
-        return lhs.id == rhs.id
-    }
-}
-
-extension UIColor {
-    func toImage(_ size: CGSize = CGSize(width: 1, height: 1)) -> UIImage {
-        let rect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-        UIGraphicsBeginImageContext(rect.size)
-        
-        if let context = UIGraphicsGetCurrentContext() {
-            context.setFillColor(self.cgColor)
-            context.fill(rect)
-            if let image = UIGraphicsGetImageFromCurrentImageContext() {
-                UIGraphicsEndImageContext()
-                return image
-            }
-        }
-        return UIImage()
-    }
-}
-
 extension Model {
     struct User {
         let id: String
@@ -128,25 +62,6 @@ extension Model {
             id = json["id"].stringValue
             login = json["login"].stringValue
             avatarURL = URL(string: json["avatar_url"].stringValue)
-        }
-    }
-}
-
-extension Model {
-    public struct Comment {
-        let id: Int
-        let user: Model.User
-        let body: String
-        let createdAt: Date?
-        let updatedAt: Date?
-        public init(json: JSON) {
-            id = json["id"].intValue
-            user = Model.User(json: json["user"])
-            body = json["body"].stringValue
-            let format = DateFormatter()
-            format.dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'"
-            createdAt = format.date(from: json["created_at"].stringValue)
-            updatedAt = format.date(from: json["updated_at"].stringValue)
         }
     }
 }
